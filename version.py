c23729d6b4fbec3adfb0fc65ac2ba692b1bef994
Machi <bruno.soarxz@gmail.com>
2026-01-28 22:26:21 -0300

    Zyris

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "zyris"
+name = "Zyris Engine"
 major = 4
 minor = 6
 patch = 0
 status = "stable"
 module_config = ""
-website = "https://godotengine.org"
+website = "https://zyrisengine.com"
 docs = "4.6"
